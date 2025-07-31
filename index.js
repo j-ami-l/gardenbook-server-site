462152c4e603018e03166bb57e71c929db4e8ac4
j-ami-l <mdhasanjamil0001@gmail.com>
2025-08-01 03:11:03 +0600

    added allgardener get method

--- index.js
+++ index.js
@@ -32,21 +32,68 @@ async function run() {
     await client.connect();
 
     const tipsCollection = client.db("GardenBook").collection("tips")
-    app.get('/browsetips' , async(req , res)=>{
-      const query = {availability : "Public" };
+    const usersCollection = client.db("GardenBook").collection("users")
+    const gardernersCollection = client.db("GardenBook").collection("gardeners")
+
+
+    app.get('/browsetips', async (req, res) => {
+      const query = { availability: "Public" };
       const result = await tipsCollection.find(query).toArray()
       res.send(result)
     })
 
 
-    app.get('/tipdetails/:id' , async (req , res)=>{
-      const id =  req.params.id
-      const query = {_id : new ObjectId(id)}
-
+    app.get('/tipdetails/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
       const result = await tipsCollection.findOne(query)
       res.send(result)
       
     })
+
+    app.post('/user', async (req , res) => {
+      const email = req.body
+      console.log(email)
+      const result = await usersCollection.findOne(email)
+      res.send(result)
+    })
+
+    app.post('/gardenbook/users', async (req, res) => {
+      const newUser = req.body;
+      const existingUser = await usersCollection.findOne({ email: newUser.email });
+      if (existingUser) {
+        return res.status(200).send({ message: "User already exists", user: existingUser });
+      }
+      const result = await usersCollection.insertOne(newUser);
+      res.status(201).send(result);
+    });
+
+    app.get('/gardeners', async (req ,res) =>{
+        const result = await gardernersCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/gardeners' ,async (req , res) =>{
+      const newGardener = req.body;
+      const result = await gardernersCollection.insertOne(newGardener)
+      res.send(result)
+
+    })
+
+
+
+    app.patch('/gardenbook/userinfo/update', async (req, res) => {
+      const email = req.body.email;
+      const id = req.body.id
+      const filter = { email : email};
+      const updateDoc = {
+        $addToSet: { likedPost: id }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+       res.send(result);
+    })
+
+    
 
     app.post('/alltips', async (req, res) => {
       const newPost = req.body;
@@ -61,35 +108,41 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/mytips/updatelikecount/:id', async (req, res) =>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const likeCount = req.body.likeCount;
+      const updateDoc = {
+        $set: { likeCount }
+      };
+      const result = await tipsCollection.updateOne(filter, updateDoc);
+       res.send(result);
+
+    })
+
+
     app.put('/mytips/update/:id', async (req, res) => {
       const id = req.params.id;
       const updatePost = req.body;
-      console.log(updatePost, id);
-
       const filter = { _id: new ObjectId(id) };
-      console.log(filter);
-      
       const options = { upsert: true };
       const updateDoc = {
         $set: updatePost,
       };
-
       const result = await tipsCollection.updateOne(filter, updateDoc, options);
-
       res.send(result)
 
 
     })
 
-    app.delete('/delete' , async (req , res) =>{
-      const {id} = req.body
-      console.log(id);
-      const query = {_id : new ObjectId({id})}
+    app.delete('/delete', async (req, res) => {
+      const { id } = req.body
+      const query = { _id: new ObjectId({ id }) }
 
       const result = tipsCollection.deleteOne(query)
       res.send(result)
-      
-    } )
+
+    })
 
 
 
